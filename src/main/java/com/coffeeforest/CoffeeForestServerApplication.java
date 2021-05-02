4121a5833c2180ad9079e80622e2592d1911729d
Yonghwi Kim <yh0921k@gmail.com>
2021-05-02 17:36:02 +0900

    [init] Coffee Forest Project 초기화
    module
    - SpringBoot   : 2.4.5
    - Database     : MariaDB, H2
    - Build Tool   : Gradle
    - Language     : Java 11
    
    start.spring.io를 이용한 프로젝트 생성
    - Dependencies : Spring Web, Spring Data JPA, Validation
                   : Lombok, H2 Database, MariaDB Driver
    
    .gitignore 추적 제외 파일 설정
    - IntelliJ, Java, Database, VisualStudioCode
    - Git, Gradle, Eclipse
    - macOS, Windows, Linux

---
+++ src/main/java/com/coffeeforest/CoffeeForestServerApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeForestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeForestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeeforest/CoffeeForestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeForestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
