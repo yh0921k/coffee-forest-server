8d5c5a20c7e6b3f70355be52d5bd44bf65a2bfc8
Yonghwi Kim <yh0921k@gmail.com>
2021-05-30 13:14:50 +0900

    [add] User 관련 도메인 로직 구현
    UserEntity.java
    Position.java
    UserRepository.java
    - 사용자 필드는 이메일, 암호, 권한, 이름, 연락처, 주소로 구성
    - 권한은 ROOT(개발자), ADMIN(관리자), EMPLOYEE(일반직원)으로 구>성
    - UserRepository는 이메일이 존재하는지 여부를 반환하는 쿼리 메서드 구현

---
+++ src/main/java/com/coffeeforest/domains/user/domain/UserRepository.java
@@ -0,0 +1,8 @@
+package com.coffeeforest.domains.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    boolean existsByEmail(String email);
+}
