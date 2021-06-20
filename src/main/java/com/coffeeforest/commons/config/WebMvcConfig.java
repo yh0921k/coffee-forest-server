4730c183efff4c05dbeaa9bd2ba76a0d44296fef
Yonghwi Kim <yh0921k@gmail.com>
2021-06-20 11:05:04 +0900

    [add] CORS 관련 설정 추가
    WebMvcConfig.java
    - 모든 Origin에 대해 GET, POST, PUT, DELETE, PATCH 메서드 관련 설정 추가

---
+++ src/main/java/com/coffeeforest/commons/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.coffeeforest.commons.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+        .maxAge(3600);
+  }
+}
