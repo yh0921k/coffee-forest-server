47d90979c47008319577f94c125cdd7b9c75a67f
Yonghwi Kim <yh0921k@gmail.com>
2021-06-20 15:44:08 +0900

    [add] 사용자 이메일과 비밀번호로 엔티티를 조회하는 로직 구현
    UserFindService.java
    UserRepository.java
    - 사용자 이메일과 비밀번호를 사용해 엔티티를 조회하는 기능 추가
    - 서비스 및 쿼리 메서드 구현

---
+++ src/main/java/com/coffeeforest/domains/user/application/UserFindService.java
@@ -0,0 +1,20 @@
+package com.coffeeforest.domains.user.application;
+
+import com.coffeeforest.domains.user.domain.UserEntity;
+import com.coffeeforest.domains.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserFindService {
+  private final UserRepository userRepository;
+
+  public UserEntity findByEmailAndPassword(String email, String password) {
+    UserEntity userEntity =
+        userRepository
+            .findByEmailAndPassword(email, password)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid Email or Password"));
+    return userEntity;
+  }
+}
--- src/main/java/com/coffeeforest/domains/user/domain/UserRepository.java
+++ src/main/java/com/coffeeforest/domains/user/domain/UserRepository.java
@@ -2,7 +2,11 @@ package com.coffeeforest.domains.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
   boolean existsByEmail(String email);
+
+  Optional<UserEntity> findByEmailAndPassword(String email, String password);
 }
