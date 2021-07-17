496b74f59d1102015a09f9d387710d173580771a
Yonghwi Kim <yh0921k@gmail.com>
2021-07-17 21:43:53 +0900

    [add] 사용자 회원가입 검증 로직 구현
    PasswordService.java
    - 정규표현식 유틸리티 클래스를 사용하여 패스워드 형식을 검증하는 메서드 구현
    - PasswordEncoder를 사용하여 클라이언트의 패스워드를 암호화 하는 메서드 구현
    - 로그인시 사용할 패스워드 검증 메서드 구현
    
    UserSignUpService.java
    - 패스워드 형식이 올바른지, 이미 등록된 계정인지 검증하는 로직 추가 구현
    - 사용자 비밀번호를 암호화하는 로직 추가 구현

--- src/main/java/com/coffeeforest/domains/user/application/UserSignUpService.java
+++ src/main/java/com/coffeeforest/domains/user/application/UserSignUpService.java
@@ -1,5 +1,8 @@
 package com.coffeeforest.domains.user.application;
 
+import com.coffeeforest.commons.exception.ExceptionState;
+import com.coffeeforest.commons.exception.detail.InvalidArgumentException;
+import com.coffeeforest.domains.auth.application.PasswordService;
 import com.coffeeforest.domains.user.application.dto.UserSaveRequest;
 import com.coffeeforest.domains.user.domain.UserEntity;
 import com.coffeeforest.domains.user.domain.UserRepository;
@@ -13,13 +16,24 @@ import javax.transaction.Transactional;
 public class UserSignUpService {
 
   private final UserRepository userRepository;
+  private final PasswordService passwordService;
 
   @Transactional
   public void signUp(UserSaveRequest userSaveRequest) {
+    String password = userSaveRequest.getPassword();
+    passwordService.validate(password);
+
+    String email = userSaveRequest.getEmail();
+    if (userRepository.existsByEmail(email)) {
+      throw new InvalidArgumentException(
+          ExceptionState.INVALID_ARGUMENT, "Email Already Registered");
+    }
+
+    String encodedPassword = passwordService.encode(password);
     userRepository.save(
         UserEntity.builder()
-            .email(userSaveRequest.getEmail())
-            .password(userSaveRequest.getPassword())
+            .email(email)
+            .password(encodedPassword)
             .name(userSaveRequest.getName())
             .phone(userSaveRequest.getPhone())
             .address(userSaveRequest.getAddress())
