1330656d42ecb90898dae92f3b73788a4a48e588
Yonghwi Kim <yh0921k@gmail.com>
2021-07-18 18:20:14 +0900

    [add] 근무 신청(직원 신청) 관련 도메인 로직 구현
    WorkApplicantEntity.java
    WorkApplicantRepository.java
    - 기업 및 직원 신청자 간 매핑 엔티티
    - 등록시 근무 상태는 단순히 '대기' 상태로 설정

---
+++ src/main/java/com/coffeeforest/domains/work_applicant/domain/WorkApplicantRepository.java
@@ -0,0 +1,5 @@
+package com.coffeeforest.domains.work_applicant.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkApplicantRepository extends JpaRepository<WorkApplicantEntity, Long> {}
