37840d0a60be9679e72e133f55e8f17be6cd0f8f
Yonghwi Kim <yh0921k@gmail.com>
2021-07-18 12:35:14 +0900

    [add] 사업자 등록번호 관련 정규표현식 패턴 추가 구현
    RegExType.java
    - 사업자 등록번호를 등록하거나 검증할 때 사용할 패턴 추가 구현
    - 사업자 등록번호는 숫자 10자리 구성이며 "XXX-XX-XXXXX"의 형태로 이루어짐

--- src/main/java/com/coffeeforest/commons/regex/RegExType.java
+++ src/main/java/com/coffeeforest/commons/regex/RegExType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RegExType {
-  PASSWORD("((?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,})");
-
+  PASSWORD("((?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,})"),
+  BUSINESS_NUMBER("^\\d{3}-\\d{2}-\\d{5}$");
   private final String pattern;
 }
