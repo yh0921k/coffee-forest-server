16328f698cd41710d63d7d7164f4be7f5beaed36
Yonghwi Kim <yh0921k@gmail.com>
2021-08-05 21:00:15 +0900

    [mod] 로그인시 상태 추가 반환
    UserSignInResponse.java
    - 근무중이며 소속된 회사가 있다면 해당 Company Index 반환, 없다면 0
    - 존재하는 회사에 근무하게 되어 등록 요청을 한 경우 WorkApplicantStatus 반환(요청이 없다면 UNKNOWN, 있다면 WAITING)
    - 기업 대표자가 서비스를 이용하며 초기 회사 등록을 진행하는 경우 해당 등록 요청이 있는지 여부를 CompanyApplicantIndex로 반환(있다면 WAIT, 없다면 UNKNOWN)
    
    UserSignInService.java
    - 이전에 구현한 서비스 클래스를 사용하여 위의 추가 상태 반환
    - CompanyEntity의 경우 WorkEntity를 우선 조회하는데, Optional로 반환되므로 WorkEntity 및 포함된 CompanyEntity가 존재하는지 여부에 따라 반환값 설정

--- src/main/java/com/coffeeforest/domains/auth/application/UserSignInService.java
+++ src/main/java/com/coffeeforest/domains/auth/application/UserSignInService.java
@@ -4,14 +4,21 @@ import com.coffeeforest.commons.jwt.AuthToken;
 import com.coffeeforest.domains.auth.application.dto.UserSignInRequest;
 import com.coffeeforest.domains.auth.application.dto.UserSignInResponse;
 import com.coffeeforest.domains.auth.application.dto.UserTokenInfo;
+import com.coffeeforest.domains.company.domain.CompanyEntity;
+import com.coffeeforest.domains.company_applicant.application.CompanyApplicantFindService;
+import com.coffeeforest.domains.company_applicant.domain.CompanyApplicantStatus;
 import com.coffeeforest.domains.user.application.UserFindService;
 import com.coffeeforest.domains.user.domain.UserEntity;
+import com.coffeeforest.domains.work.application.WorkFindService;
+import com.coffeeforest.domains.work.domain.WorkStatus;
+import com.coffeeforest.domains.work_applicant.application.WorkApplicantFindService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,9 +26,13 @@ public class UserSignInService {
   private final UserFindService userFindService;
   private final UserAuthService userAuthService;
   private final PasswordService passwordService;
+  private final WorkApplicantFindService workApplicantFindService;
+  private final CompanyApplicantFindService companyApplicantFindService;
+  private final WorkFindService workFindService;
 
   @Transactional
-  public UserSignInResponse signIn(UserSignInRequest userSignInRequest, HttpServletResponse httpServletResponse) {
+  public UserSignInResponse signIn(
+      UserSignInRequest userSignInRequest, HttpServletResponse httpServletResponse) {
     UserEntity userEntity = userFindService.findByEmail(userSignInRequest.getEmail());
     passwordService.matches(userEntity.getPassword(), userSignInRequest.getPassword());
 
@@ -35,6 +46,18 @@ public class UserSignInService {
     cookie.setMaxAge(60 * 2);
     httpServletResponse.addCookie(cookie);
 
+    WorkStatus workStatus =
+        workApplicantFindService.existsByWorkStatus(userEntity.getId(), WorkStatus.WAITING);
+
+    CompanyApplicantStatus companyApplicantStatus =
+        companyApplicantFindService.existsByCompanyApplicantStatus(
+            userEntity.getId(), CompanyApplicantStatus.WAIT);
+
+    Optional<CompanyEntity> companyEntity =
+        workFindService
+            .findByUserIndexAndWorkStatus(userEntity.getId(), WorkStatus.WORKING)
+            .map(entity -> entity.getCompanyEntity());
+
     return UserSignInResponse.builder()
         .userIndex(userEntity.getId())
         .email(userEntity.getEmail())
@@ -47,6 +70,9 @@ public class UserSignInService {
                 .accessToken(accessToken.getValue())
                 .refreshToken(refreshToken.getValue())
                 .build())
+        .companyIndex(companyEntity.isPresent() ? companyEntity.get().getId() : 0)
+        .workApplicantStatus(workStatus)
+        .companyApplicantStatus(companyApplicantStatus)
         .build();
   }
 }
--- src/main/java/com/coffeeforest/domains/auth/application/dto/UserSignInResponse.java
+++ src/main/java/com/coffeeforest/domains/auth/application/dto/UserSignInResponse.java
@@ -1,6 +1,8 @@
 package com.coffeeforest.domains.auth.application.dto;
 
+import com.coffeeforest.domains.company_applicant.domain.CompanyApplicantStatus;
 import com.coffeeforest.domains.user.domain.Position;
+import com.coffeeforest.domains.work.domain.WorkStatus;
 import lombok.*;
 
 @Getter
@@ -14,4 +16,8 @@ public class UserSignInResponse {
   private Position position;
   private String profileImage;
   private UserTokenInfo userTokenInfo;
+
+  private Long companyIndex;
+  private WorkStatus workApplicantStatus;
+  private CompanyApplicantStatus companyApplicantStatus;
 }
