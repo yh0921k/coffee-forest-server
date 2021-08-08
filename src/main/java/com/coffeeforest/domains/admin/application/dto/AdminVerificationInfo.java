77cac21a7beaaa957cbfe5bc18bbdce2bbdcc1f6
Yonghwi Kim <yh0921k@gmail.com>
2021-08-08 18:37:05 +0900

    [add] Admin 권한 검증 로직 분리
    AdminVerificationInfo.java
    - Admin 권한 검증시 기본적으로 어떤 기업에 속한 어떤 유저의 권한을 검증해야 하므로 Company Index, User Index를 요청 파라미터로 받음
    
    AdminVerificationService.java
    - 요청 파라미터를 사용해 Work Entity를 조회하여 해당 기업에 속한 유저가 맞는지 1차 검증
    - 해당 Work Entity에서 User Entity를 조회하여 Admin 권한을 가지고 있는지 2차 검증

---
+++ src/main/java/com/coffeeforest/domains/admin/application/dto/AdminVerificationInfo.java
@@ -0,0 +1,9 @@
+package com.coffeeforest.domains.admin.application.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AdminVerificationInfo {
+  private Long companyIndex;
+  private Long userIndex;
+}
