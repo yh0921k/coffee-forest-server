a971df295336cc562ed2e2cca3ae19b0f15ea39d
Yonghwi Kim <yh0921k@gmail.com>
2021-08-15 16:55:18 +0900

    [add] 연락처 유효성 검사를 위한 코드 구현
    RegExType.java
    - 연락처는 XXX-XXXX-XXXX 형태로 구성되어야함

--- src/main/java/com/coffeeforest/commons/regex/RegExType.java
+++ src/main/java/com/coffeeforest/commons/regex/RegExType.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum RegExType {
   PASSWORD("((?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,})"),
-  BUSINESS_NUMBER("^\\d{3}-\\d{2}-\\d{5}$");
+  BUSINESS_NUMBER("^\\d{3}-\\d{2}-\\d{5}$"),
+  PHONE_NUMBER("^\\d{3}-\\d{4}-\\d{4}$");
+
   private final String pattern;
 }
