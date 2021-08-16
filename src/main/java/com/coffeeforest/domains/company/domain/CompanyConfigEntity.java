ca0ee62492c76602c6561b816cf6c9f7dbd6bb10
Yonghwi Kim <yh0921k@gmail.com>
2021-08-16 16:54:02 +0900

    [add] 기업 설정 관련 엔티티 구현
    CompanyConfigEntity.java
    CompanyConfigRepository.java
    - 근무 시작 시간, 근무 종료 시간
    - 점심 시작 시간, 점심 종료 시간
    - 탄력 근무제 여부 및 탄력 근무제 출근 범위
    - 1년 미만 및 이상 근무자에 대한 연차 개수
    - 오전 반차 시작 및 종료 시간
    - 오후 반차 시작 및 종료 시간
    
    - 기업 설정은 위의 필드로 구성됨
    - 초기 기본 값을 설정해주며 추후 수정 가능

---
+++ src/main/java/com/coffeeforest/domains/company/domain/CompanyConfigEntity.java
@@ -0,0 +1,57 @@
+package com.coffeeforest.domains.company.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+
+@Getter
+@Table(name = "company_config")
+@Entity
+public class CompanyConfigEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private LocalTime workStartTime;
+  private LocalTime workEndTime;
+
+  private LocalTime lunchStartTime;
+  private LocalTime lunchEndTime;
+
+  private boolean flexibleState;
+  private LocalTime flexibleWorkStartTime;
+  private LocalTime flexibleWorkEndTime;
+
+  private int fullDayOffUpperYear;
+  private int fullDayOffUnderYear;
+
+  private LocalTime morningDayOffStartTime;
+  private LocalTime morningDayOffEndTime;
+
+  private LocalTime afternoonDayOffStartTime;
+  private LocalTime afternoonDayOffEndTime;
+
+  @Builder
+  public CompanyConfigEntity() {
+    this.workStartTime = LocalTime.of(9, 0);
+    this.workEndTime = LocalTime.of(18, 0);
+
+    this.lunchStartTime = LocalTime.of(12, 0);
+    this.lunchEndTime = LocalTime.of(13, 0);
+
+    this.flexibleState = false;
+    this.flexibleWorkStartTime = LocalTime.of(8, 0);
+    this.flexibleWorkEndTime = LocalTime.of(10, 0);
+
+    this.fullDayOffUnderYear = 15;
+    this.fullDayOffUnderYear = 1;
+
+    this.morningDayOffStartTime = LocalTime.of(9, 0);
+    this.morningDayOffEndTime = LocalTime.of(13, 30);
+
+    this.afternoonDayOffStartTime = LocalTime.of(13, 30);
+    this.afternoonDayOffEndTime = LocalTime.of(18, 0);
+  }
+}
